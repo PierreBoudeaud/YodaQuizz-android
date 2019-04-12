93247caa4cf701e499e832360e5702753d93579f
valoo22 <valentin_dejoue@live.fr>
2019-04-12 16:48:42 +0200

    Add Function 'switchLayout'
    Creating function who switch a main_content of application from the navigation view

--- app/src/main/java/fr/eni/geekoquizz/activity/MainActivity.java
+++ app/src/main/java/fr/eni/geekoquizz/activity/MainActivity.java
@@ -3,23 +3,22 @@ package fr.eni.geekoquizz.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.ImageButton;
 
 import fr.eni.geekoquizz.R;
-import fr.eni.geekoquizz.adapter.QuizzAdapter;
+import fr.eni.geekoquizz.adapter.ListQuizzAdapter;
+import fr.eni.geekoquizz.adapter.ModifQuizzAdapter;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -38,33 +37,13 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
 
-
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         navigationView.getMenu().getItem(0).setChecked(true);
         this.setTitle(R.string.title_activity_list_quizz);
 
-        RecyclerView recyclerViewQuizz = (RecyclerView) findViewById(R.id.rvQuizz);
-        recyclerViewQuizz.setHasFixedSize(true);
-
-        LayoutManagerQuizz = new LinearLayoutManager(this);
-
-        DividerItemDecoration divider = new DividerItemDecoration(recyclerViewQuizz.getContext(),DividerItemDecoration.VERTICAL);
-
-        recyclerViewQuizz.setLayoutManager(LayoutManagerQuizz);
-
-        String[][] infos = {
-                {"-700094","-700095","-700096","Jeux-vidéo années 90","50","2.5f","QCM (1 réponse possible)","-700002","-700002","-700002","-700002","-700002","Valoo22","12/04/2019","Description : Redécouvrez les jeux-vidéo de son age d'or grâce a ce petit quizz développé par mes soins."},
-                {"-700094","-700095","-700096","Jeux-vidéo années 90","50","2.5f","QCM (1 réponse possible)","-700002","-700002","-700002","-700002","-700002","Valoo22","12/04/2019","Description : Redécouvrez les jeux-vidéo de son age d'or grâce a ce petit quizz développé par mes soins."}
-        };
-
-        RecyclerView.Adapter AdapterQuizz = new QuizzAdapter(infos);
-
-        recyclerViewQuizz.setAdapter(AdapterQuizz);
-
-        recyclerViewQuizz.addItemDecoration(divider);
+        switchLayout(R.id.nav_list_quizz);
 
         toggle.syncState();
     }
@@ -88,20 +67,20 @@ public class MainActivity extends AppCompatActivity
         switch (id)
         {
             case R.id.nav_list_quizz:
-
+                switchLayout(R.id.nav_list_quizz);
                 break;
             case R.id.nav_modif_quizz:
-
+                switchLayout(R.id.nav_modif_quizz);
+                break;
+            case R.id.nav_theme:
+                switchLayout(R.id.nav_theme);
+                break;
+            case R.id.nav_type_de_quizz:
+                switchLayout(R.id.nav_type_de_quizz);
                 break;
             case R.id.nav_parametre:
                 Intent intent = new Intent(this,SettingsActivity.class);
                 startActivity(intent);
-                break;
-            case R.id.nav_theme:
-
-                break;
-            case R.id.nav_type_de_quizz:
-
                 break;
             default:
 
@@ -110,5 +89,41 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void switchLayout(int idItem)
+    {
+        String[][] infos = {
+                {"-700094","-700095","-700096","Jeux-vidéo années 90","50","2.5f","QCM (1 réponse possible)","-700002","-700002","-700002","-700002","-700002","Valoo22","12/04/2019","Description : Redécouvrez les jeux-vidéo de son age d'or grâce a ce petit quizz développé par mes soins."},
+                {"-700094","-700095","-700096","Jeux-vidéo années 90","50","2.5f","QCM (1 réponse possible)","-700002","-700002","-700002","-700002","-700002","Valoo22","12/04/2019","Description : Redécouvrez les jeux-vidéo de son age d'or grâce a ce petit quizz développé par mes soins."}
+        };
+        RecyclerView recyclerViewQuizz = (RecyclerView) findViewById(R.id.rvQuizz);
+        ImageButton btnAddQuizz = (ImageButton)findViewById(R.id.btnAddQuizz);
+        recyclerViewQuizz.setHasFixedSize(true);
+        LayoutManagerQuizz = new LinearLayoutManager(this);
+        DividerItemDecoration divider = new DividerItemDecoration(recyclerViewQuizz.getContext(),DividerItemDecoration.VERTICAL);
+        recyclerViewQuizz.addItemDecoration(divider);
+        recyclerViewQuizz.setLayoutManager(LayoutManagerQuizz);
+        switch(idItem)
+        {
+            case R.id.nav_list_quizz :
+                this.setTitle("Liste des Quizz");
+                RecyclerView.Adapter LisyAdapterQuizz = new ListQuizzAdapter(infos);
+                recyclerViewQuizz.setAdapter(LisyAdapterQuizz);
+                btnAddQuizz.setVisibility(View.GONE);
+                break;
+            case R.id.nav_modif_quizz :
+                this.setTitle("Modifier un Quizz");
+                RecyclerView.Adapter ModifAdapterQuizz = new ModifQuizzAdapter(infos);
+                recyclerViewQuizz.setAdapter(ModifAdapterQuizz);
+                btnAddQuizz.setVisibility(View.VISIBLE);
+                break;
+            case R.id.nav_theme :
+
+                break;
+            case R.id.nav_type_de_quizz :
+
+                break;
+        }
     }
 }
