b60a08759206d01e903a5dd92e2a3ce9c35a16c0
Gitea <gitea@fake.local>
2019-04-15 15:16:02 +0200

    Add reponse model

---
+++ app/src/main/java/fr/eni/geekoquizz/bo/Reponse.java
@@ -0,0 +1,74 @@
+package fr.eni.geekoquizz.bo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Reponse implements Serializable {
+
+    private int id;
+
+    private String nom;
+
+    private Date dateCrea;
+
+    private boolean correct;
+
+    public Reponse() {
+        this.dateCrea = new Date();
+    }
+
+    public Reponse(String nom, boolean correct) {
+        this();
+        this.nom = nom;
+        this.correct = correct;
+    }
+
+    public Reponse(int id, String nom, Date dateCrea, boolean correct) {
+        this.id = id;
+        this.nom = nom;
+        this.dateCrea = dateCrea;
+        this.correct = correct;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDateCrea() {
+        return dateCrea;
+    }
+
+    public void setDateCrea(Date dateCrea) {
+        this.dateCrea = dateCrea;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Reponse{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", dateCrea=" + dateCrea +
+                ", correct=" + correct +
+                '}';
+    }
+}
