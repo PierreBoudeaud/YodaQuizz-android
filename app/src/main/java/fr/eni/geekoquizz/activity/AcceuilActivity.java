b03fc3c3b72708af3ba172208fbc8bf562e5b35c
Pierre Boudeaud <pboudeaud@gmail.com>
2019-04-26 14:23:18 +0200

    Upgrade import

--- app/src/main/java/fr/eni/geekoquizz/activity/AcceuilActivity.java
+++ app/src/main/java/fr/eni/geekoquizz/activity/AcceuilActivity.java
@@ -1,21 +1,37 @@
 package fr.eni.geekoquizz.activity;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
 import fr.eni.geekoquizz.R;
+import fr.eni.geekoquizz.bo.Quizz;
+import fr.eni.geekoquizz.dao.Bdd;
 import fr.eni.geekoquizz.service.ImportService;
+import fr.eni.geekoquizz.tools.Network;
+import fr.eni.geekoquizz.view_model.QuizzViewModel;
 
 public class AcceuilActivity extends AppCompatActivity {
-
-    private static boolean isNotImported = true;
     ImageView ivGif;
     CountDownTimer timer;
 
@@ -37,33 +53,64 @@ public class AcceuilActivity extends AppCompatActivity {
                 AcceuilActivity.this.startActivity(intent);
             }
         }.start();*/
-
-        if(isNotImported) {
-            isNotImported = !isNotImported;
-            ImportService importService = new ImportService(this);
-            AsyncImportTask task = new AsyncImportTask();
-            task.execute(importService);
-            importService.importAll();
-            Intent intent = new Intent(AcceuilActivity.this, MainActivity.class);
-            AcceuilActivity.this.startActivity(intent);
-        } else {
-            Intent intent = new Intent(AcceuilActivity.this, MainActivity.class);
-            AcceuilActivity.this.startActivity(intent);
-        }
-
     }
 
-    public class AsyncImportTask extends AsyncTask<ImportService, Void, Void> {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(isNotUpdate()) {
+            if(Network.isAvailable(this)) {
+                if(isNotUpdate()) {
+                    this.deleteDatabase("GeekoQuizz");
+                    ImportService importService = new ImportService(this);
+                    importService.importAll();
+                    setUpdate();
+                }
 
-        @Override
-        protected Void doInBackground(ImportService... importServices) {
-            return null;
+            } else {
+                Toast.makeText(this, "Aucune connection internet détecté\nImport impossible", Toast.LENGTH_LONG);
+                return;
+            }
         }
+        Intent intent = new Intent(AcceuilActivity.this, MainActivity.class);
+        AcceuilActivity.this.startActivity(intent);
+    }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
+    private static final String goodFileContents = "1";
 
+    private boolean isNotUpdate() {
+        String filename = "isUpdate";
+        String content = "";
+        try {
+            FileInputStream inputStream = openFileInput(filename);
+            StringBuffer fileContent = new StringBuffer("");
+
+            byte[] buffer = new byte[1024];
+            int n;
+
+            while ((n = inputStream.read(buffer)) != -1)
+            {
+                fileContent.append(new String(buffer, 0, n));
+            }
+            content = fileContent.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return !content.equals(goodFileContents);
+    }
+
+    private void setUpdate() {
+        String filename = "isUpdate";
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(goodFileContents.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
